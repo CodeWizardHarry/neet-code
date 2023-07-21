b059435d45185e941cad9e910a3478ed0dca870a
Harry <97231806+harryz7878@users.noreply.github.com>
2023-07-21 23:41:49 +0100

    update

--- python/harrys_solutions/0110-balanced-binary-tree.py
+++ python/harrys_solutions/0110-balanced-binary-tree.py
@@ -12,40 +12,27 @@ class TreeNode:
 
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        """
+        Given a binary tree, determine if it is height-balanced.
+
+        Steps:
+        1. start from the bottom of the tree and apply dfs
+           to check if each subtree is height-balanced
+        2. determine if abs(left.height - right.height) <= 1
+        2. create an array for each dsf to track [balance_status, height of current tree]
+        3.
+
+        Returns: True if it's height-balanced tree, otherwise return false
+        """
+
         def dfs(root):
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
             if not root:
                 return [True, 0]
 
             left, right = dfs(root.left), dfs(root.right)
             balanced = left[0] and right[0] and abs(left[1] - right[1]) <= 1
-            return [
-                balanced,
-                1 + max(left[1], right[1]),
-            ]  # tracks the height of the node , height of a tree if the number of layers in a tree
+            height = 1 + max(left, right)  # tracks height of the current node, height of a tree is the number of layers in a tree
+
+            return [balanced, height]
 
         return dfs(root)[0]
